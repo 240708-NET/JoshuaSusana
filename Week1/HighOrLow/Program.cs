492458b4ff33649a11f169564ccb97127f935a08
Josh <kuyang27@gmail.com>
2024-07-10 17:33:57 -0400

    Updating HighOrLow program to be OOP

--- Week1/HighOrLow/Program.cs
+++ Week1/HighOrLow/Program.cs
@@ -1,40 +1,16 @@
 class Program
 {
-	static void Main(string[]args)
+	static void Main( string[] args )
 	{
-		//Declaring Variables
-		int targetNumber;
-		int guessNumber;
-		int roundCount;
-		string guessString;
+		Console.WriteLine( "High/Low Running" );
 
-		roundCount = 0;
+		// Singleton - you create a single instance of a thing that is referenced throughout the application to complete some functionality.
+		// create an instance of the Game class
+		Game newGame = new Game(); 
 
-		Random rand = new Random();
-		targetNumber = rand.Next(11);
+		int roundCount = newGame.PlayGame();
 
-		do {
-			roundCount++;			
-
-			Console.Write("Please enter a guess between -1 and 11: ");
-			guessString = Console.ReadLine();
-
-			guessNumber = Int32.Parse(guessString);
-
-			if(guessNumber == targetNumber)
-			{
-				Console.WriteLine("Congratulations! You Win!");
-			}
-			else if(guessNumber > targetNumber)
-			{
-				Console.WriteLine("Your guess was higher than the target number.");
-			}
-			else
-			{
-				Console.WriteLine("Your guess was lower than the target number.");
-			}
-		} while(guessNumber != targetNumber);
-		Console.WriteLine("Thanks for playing!");
-		Console.WriteLine("It took you {0} rounds to guess the number.", roundCount);
-	}
+		Console.WriteLine( "Thanks for playing!" );
+		Console.WriteLine( "You took {0} rounds to guess the answer!", roundCount );
+	}	
 }
