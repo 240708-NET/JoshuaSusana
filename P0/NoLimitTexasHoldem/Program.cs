f883796add812aecf04bd2aadf97784fe39a997b
Josh <kuyang27@gmail.com>
2024-07-11 11:55:12 -0400

    Added some logic in Program.cs using the Card and Deck classes

--- P0/NoLimitTexasHoldem/Program.cs
+++ P0/NoLimitTexasHoldem/Program.cs
@@ -1,7 +1,35 @@
-class Program
+namespace NoLimitTexasHoldem
 {
-    static void Main(string[] args)
+    class Program
     {
-        
+        static void Main(string[] args)
+        {
+            //Instantiating a new deck
+            Deck deck = new Deck();
+
+            deck.Shuffle();
+
+            //Dealing hole cards to player and machine
+            Card playerCard1 = deck.Deal();
+            Card machineCard1 = deck.Deal();
+            Card playerCard2 = deck.Deal();
+            Card machineCard2 = deck.Deal();
+
+            //Outputting what the player and machine's hole cards are
+            Console.WriteLine($"Player has {playerCard1} and {playerCard2}");
+            Console.WriteLine($"Machine has {machineCard1} and {machineCard2}");
+
+            //Dealing flop, turn, and river in way it would be done in real life
+            Card burnCard1 = deck.Deal();
+            Card communityCard1 = deck.Deal();
+            Card communityCard2 = deck.Deal();
+            Card communityCard3 = deck.Deal();
+            Card burnCard2 = deck.Deal();
+            Card communityCard4 = deck.Deal();
+            Card burnCard3 = deck.Deal();
+            Card communityCard5 = deck.Deal();
+
+            Console.WriteLine($"The community cards are \n {communityCard1} \n {communityCard2} \n {communityCard3} \n {communityCard4} \n {communityCard5}");
+        }
     }
 }
