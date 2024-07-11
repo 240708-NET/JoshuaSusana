5bafb620f56b03fb663f11cc3501b9485e1d4bf3
Josh <kuyang27@gmail.com>
2024-07-10 23:16:49 -0400

    Implented my first classes, which were the Card and Deck classes

---
+++ P0/NoLimitTexasHoldem/Card.cs
@@ -0,0 +1,23 @@
+namespace NoLimitTexasHoldem
+{
+    public class Card
+    {
+        //Attributes of a card include its rank (Ace, queen, etc.) and its suit (Spades, hearts, etc.)
+        public string Rank { get; set; }
+        public string Suit { get; set; }
+        
+        //Constructor that specifies exact card
+        public Card(string suit, string rank)
+        {
+            Suit = suit;
+            Rank = rank;
+        }
+
+        //To have a string representation for the object, overrode the method to read better for the user
+        //Would otherwise just be "NoLimitTexasHoldem.Card"
+        public override string ToString()
+        {
+            return $"{Rank} of {Suit}";
+        }
+    }
+}
---
+++ P0/NoLimitTexasHoldem/Deck.cs
@@ -0,0 +1,65 @@
+using System;       //For the random class
+using System.Collections.Generic;       //To be able to use Lists
+
+namespace NoLimitTexasHoldem
+{
+    public class Deck
+    {
+        //Attributes of a deck include the cards and a random object to be able to shuffle
+        private List<Card> cards;
+        private Random random;
+
+        public Deck()
+        {
+            //Creating a List object that holds Card objects
+            cards = new List<Card>();
+
+            //Note that these are in the constructor instead of with the other attributes since I do not expect the arrays to be
+            //needed anywhere else but in initialization
+            string[] suits = { "Spades", "Hearts", "Diamonds", "Clubs" };
+            string[] ranks = { "2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K", "A" };
+
+            //For each card that exists in a standard deck of playing cards, add it to the deck
+            foreach (string suit in suits)
+            {
+                foreach (string rank in ranks)
+                {
+                    cards.Add(new Card(suit, rank));
+                }
+            }
+
+            //Instantiating a Random object
+            random = new Random();
+        }
+
+        public void Shuffle()
+        {
+            for(int i=0; i<52; i++)
+            {
+                //Generate a random number between 0 and i+1
+                int j = random.Next(i + 1); 
+                //Using temp method to swap cards an indices i and j
+                Card temp = cards[i];       
+                cards[i] = cards[j];
+                cards[j] = temp;
+
+                //This uses the Fisher-Yates, one of the first things that came up for me in my online lookups
+            }
+        }
+
+        public Card Deal()
+        {
+            //Cannot deal an empty deck of cards
+            if (cards.Count == 0)
+            {
+                 return null;
+            }
+            //Actually get the card before removing so we can return it
+            Card card = cards[0];
+
+            //We can always remove at indice 0 since a List always shifts everything to the left and dynamically resizes
+            cards.RemoveAt(0);
+            return card;
+        }
+    }
+}
